6497bf66691ed58f08f4b63695da7408566b5a0d
farras <farrasfadhilah06@gmail.com>
2026-03-12 00:15:56 +0700

    ...

--- database/migrations/2026_03_11_070941_add_timestamps_to_items_table.php
+++ database/migrations/2026_03_11_070941_add_timestamps_to_items_table.php
@@ -9,12 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('items', function (Blueprint $table) {
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('items', function (Blueprint $table) {
+        $table->id();
+        $table->string('item_code')->unique(); // ID Panjang sesuai Figma
+        $table->string('name');
+        $table->enum('category', ['Elektronik', 'Furniture', 'Perlengkapan Ibadah', 'Sarana Umum']);
+        $table->enum('status', ['Available', 'Borrowed', 'Maintenance', 'Damaged'])->default('Available');
+        $table->string('location')->nullable();
+        $table->string('image_url')->default('default_item.png');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::create([
+        'name' => 'Gofur Aryan',
+        'email' => 'gofuraryann@gmail.com',
+        'role' => 'petugas',
+        'phone' => '+62859180375715',
+        'password' => bcrypt('password123'),
+    ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    \App\Models\User::create([
+        'name' => 'Ryan',
+        'email' => 'gofuryan35@gmail.com',
+        'role' => 'warga',
+        'phone' => '859180375715',
+        'password' => bcrypt('password123'),
+    ]);
+
+    // Masukkan Data Items sesuai Figma
+    \App\Models\Item::create([
+        'item_code' => 'ELC-MIC-001',
+        'name' => 'Mic Wireless Shure',
+        'category' => 'Elektronik',
+        'status' => 'Available',
+        'location' => 'Ruang Operator'
+    ]);
     }
 }
